fc1f2a2d90299aac2e5a31fcdfc8db71999d112e
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2021-09-24 20:14:51 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+/ngrok tcp 3389 &
+sleep 5000
+bash -c /usr/bin/supervisord -c /etc/supervisor/supervisord.conf &
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
